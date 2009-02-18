fd91feeef603c12f735ea08303cd1a693cb99bb8
Gert Thiel <GertThiel@gmx.net>
2009-02-19 03:15:48 +0800

    * Added an updated at field * Changed type of file size to integer
    Signed-off-by: Tom Locke <tom@tomlocke.com>

--- init.rb
+++ init.rb
@@ -8,7 +8,8 @@ module ::Paperclip
       fields do |f|
         f.field "#{name}_file_name",    :string
         f.field "#{name}_content_type", :string
-        f.field "#{name}_file_size",    :string
+        f.field "#{name}_file_size",    :integer
+        f.field "#{name}_updated_at",   :datetime
       end
     
       declare_attr_type name, ::Paperclip::Attachment
